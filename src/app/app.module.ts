eaa217d8935668a27e39c9db6200963e3c656f19
KellyEgesa <kelly.egesa@gmail.com>
2020-11-14 23:10:59 +0300

    Added a form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DisplayProfileComponent } from './display-profile/display-profile.component';
 import { FormsModule } from '@angular/forms';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
-  declarations: [AppComponent, DisplayProfileComponent],
+  declarations: [AppComponent, DisplayProfileComponent, FormComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
