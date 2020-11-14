a30f0f3b02ab29f7879b39ed6460c95f38027fb2
KellyEgesa <kelly.egesa@gmail.com>
2020-11-15 00:11:24 +0300

    made a repositories component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { AppComponent } from './app.component';
 import { DisplayProfileComponent } from './display-profile/display-profile.component';
 import { FormsModule } from '@angular/forms';
 import { FormComponent } from './form/form.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
-  declarations: [AppComponent, DisplayProfileComponent, FormComponent],
+  declarations: [AppComponent, DisplayProfileComponent, FormComponent, RepositoriesComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
